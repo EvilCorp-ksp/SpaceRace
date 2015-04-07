b3319146b4f300fca63056419d01d47cb54e6ab7
johnqevil <ryan.pershing@gmail.com>
2015-04-07 10:34:04 -0700

    Revert "Revert "Incremental""
    This reverts commit 023670f2eee45acf092a62be47f9135eb81bec31.

--- SRTraining.cs
+++ SRTraining.cs
@@ -7,6 +7,7 @@ namespace SpaceRace
 {
     public class SRTraining
     {
+        private static bool unlocked = false;
         public static void MissionSim(Simulation sim, string name)
         {
             foreach (ProtoCrewMember c in HighLogic.CurrentGame.CrewRoster.Crew)
@@ -22,20 +23,28 @@ namespace SpaceRace
         public static void BuildList()
         {
             SpaceRaceMain.simulations.Clear();
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Kerbin", type = FlightLog.EntryType.Flight });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Kerbin", type = FlightLog.EntryType.Orbit });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Kerbin", type = FlightLog.EntryType.Land });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Flight });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Orbit });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Land });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Flight });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Orbit });
-            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Land });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Kerbin", type = FlightLog.EntryType.Flight, description = "Flight over Kerbin" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Kerbin", type = FlightLog.EntryType.Orbit, description = "Orbit around Kerbin" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Flight, description = "Flight over the Mun" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Orbit, description = "Orbit around the Mun" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Mun", type = FlightLog.EntryType.Land, description = "Landing on the Mun" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Flight, description = "Flight over Minmus" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Orbit, description = "Orbit around Minmus" });
+            SpaceRaceMain.simulations.Add(new Simulation { body = "Minmus", type = FlightLog.EntryType.Land, description = "Landing on Minmus" });
+            if (unlocked == true)
+            {
+                SpaceRaceMain.simulations.Add(new Simulation { body = "Duna", type = FlightLog.EntryType.Flight, description = "Flight over Duna" });
+                SpaceRaceMain.simulations.Add(new Simulation { body = "Duna", type = FlightLog.EntryType.Orbit, description = "Orbit around Duna" });
+                SpaceRaceMain.simulations.Add(new Simulation { body = "Duna", type = FlightLog.EntryType.Land, description = "Landing on Duna" });
+
+            }
         }
 }
     public class Simulation
     {
         public string body { get; set; }
+        public string description { get; set; }
+        public int xp { get; set; }
         public FlightLog.EntryType type { get; set; }
     }
 
